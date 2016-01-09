34c456fde1c3a20210daafb95fec359196a81d97
Andre <andre.github@gmail.com>
2016-01-10 07:45:43 +0800

    add local env files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 var http = require('http');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 http.createServer(function(request, response){
     response.writeHead(200, {"Content-Type": "text/plain"});
