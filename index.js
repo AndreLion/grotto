decfc1c643625b87a11ddac5e6045efe730b201d
Andre <andre.github@gmail.com>
2016-01-10 12:50:44 +0800

    add log

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ http.createServer(function(request, response){
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("It's alive!");
     response.end();
-    log.log('request');
+    log.log(request.url);
+    log.log(request.headers);
 }).listen(PORT, function(){
     log.info('Listening Port: ' + PORT);
     log.info('Server listening on: http://localhost:%s', PORT);
