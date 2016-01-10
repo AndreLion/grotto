e946110eb0fc1a9bb49c22807d26ad55be18f6ac
Andre <andre.github@gmail.com>
2016-01-10 12:29:13 +0800

    add log

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
  * Created by andliu2 on 1/10/16.
  */
 
-var http = require('http');
+var http = require('http'),
+    Logger = require('le_node'),
+    log;
+
 const PORT = process.env.PORT;
+
+log = new Logger({
+    token:'c63ea773-e436-4faf-a5d9-da09789c42c3'
+});
+
 
 http.createServer(function(request, response){
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("It's alive!");
     response.end();
-}).listen(PORT);
+}).listen(PORT, function(){
+    log.info('Listening Port: ' + PORT);
+    log.info('Server listening on: http://localhost:%s', PORT);
+});
