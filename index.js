b6278b2be88aa8bf889aa5faa81520cd7682e65a
Andre <andre.github@gmail.com>
2016-01-11 18:19:12 +0800

    Test nginx dedirect config

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT;
 
 http.createServer(function(request, response){
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("It's alive!");
+    response.write("It's nginx!");
     response.end();
     log.log(request.url);
     log.log(request.headers);
